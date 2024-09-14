9d5cdbccd84b5b2b20668e8f0dd94e58ddc90c20
HabaAndrei <habaconstantin45@gmail.com>
2024-09-14 15:14:51 +0300

    see the days with activities

--- Screens/Program.js
+++ Screens/Program.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import {formatDateFromMilliseconds, address_function_program} from '../diverse.js';
-import { Spinner, Center } from "@gluestack-ui/themed";
+import { ArrowRightIcon, Spinner, Center, Card, Heading, Link, LinkText, HStack, Image, Icon } from "@gluestack-ui/themed";
 import axios from 'axios';
 
 
 const Program = (props) => {
 
-    const [program, setProgram] = useState(false);
+    const [program, setProgram] = useState([]);
 
 
     const fromDate = '14-09-2024';
@@ -29,7 +29,8 @@ const Program = (props) => {
         axios.post(`${address_function_program}`, 
             {  fromDate, toDate, city, country, newCheckbox
         }).then((data)=>{
-            console.log(data.data);
+            const val = Object.values(data.data.program);
+            setProgram(val)
         }).catch((err)=>{
             console.log(err);
         })
@@ -39,19 +40,70 @@ const Program = (props) => {
 
 
   return (
-    <View>
+    <ScrollView  style={{marginTop: 40, marginBottom: 40}} >
 
-        {!program ? 
+        {!program.length ? 
             <View style={styles.container} >
                 <Center  >
                     <Spinner color="$indigo600" />
                 </Center>
             </View> :
             <View  >
-                <Text>Program</Text>
+                
+            {program.map((ob, index)=>{
+                console.log(ob);
+                return  <Card  key={index}  p="$5" borderRadius="$lg" maxWidth={360} m="$3">
+      
+                    <Text
+                    fontSize="$sm"
+                    fontStyle="normal"
+                    fontFamily="$heading"
+                    fontWeight="$normal"
+                    lineHeight="$sm"
+                    mb="$2"
+                    sx={{
+                        color: "$textLight700",
+                        _dark: {
+                        color: "$textDark200",
+                        },
+                    }}
+                    >
+                       {'Day' + ob.day + " | " } {ob.date}  
+                    </Text>
+                    <Heading size="md" fontFamily="$heading" mb="$4">
+                        {ob.title}
+                    </Heading>
+                    <Link href="https://v1.gluestack.io/" isExternal>
+                        <HStack alignItems="center">
+                            <LinkText
+                            size="sm"
+                            fontFamily="$heading"
+                            fontWeight="$semibold"
+                            color="$primary600"
+                            $dark-color="$primary300"
+                            textDecorationLine="none"
+                            >
+                                See full day
+                            </LinkText>
+                            <Icon
+                            as={ArrowRightIcon}
+                            size="sm"
+                            color="$primary600"
+                            mt="$0.5"
+                            ml="$0.5"
+                            $dark-color="$primary300"
+                            />
+                        </HStack>
+                    </Link>
+                </Card>
+               
+            })}
+            
+
+
             </View>
         }
-    </View>
+    </ScrollView>
   )
 }
 
