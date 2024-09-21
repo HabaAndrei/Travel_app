1747b0f1117806421904d9d389531da9036c33ea
HabaAndrei <habaconstantin45@gmail.com>
2024-09-21 17:44:32 +0300

    tesing modify program with my azure function

--- Screens/Program.js
+++ Screens/Program.js
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native'
 import React, {useState, useEffect} from 'react'
-import {checkWebsites, address_function_program} from '../diverse.js';
+import {address_function_program} from '../diverse.js';
 import { ArrowRightIcon, Spinner, Center, Card, Heading, Link, LinkText, HStack, Image, Icon } from "@gluestack-ui/themed";
 import axios from 'axios';
 import ModalDayProgram from '../Components/ModalDayProgram.js';
 
 const Program = (props) => {
 
-  const [modalVisible, setModalVisible] = useState({isOpen:false, data:{}});
+  // observation | create 
 
+  const [modalVisible, setModalVisible] = useState({isOpen:false, data:{}});
+  const [observation, setObservation] = useState('');
   const [program, setProgram] = useState([]);
 
   const prog = {
@@ -59,113 +61,133 @@ const Program = (props) => {
         }
       ]
     },
-    3:{
-      day: 3,
-      date: "22-09-2024",
-      title: "Nature and Outdoors",
-      activities: [
-        {
-          place: "Hyde Park",
-          address: "Hyde Park, London, United Kingdom",
-          description: "One of London's largest and most famous parks.",
-          info: "Free entry.",
-          link: "https://www.royalparks.org.uk/parks/hyde-park",
-          time: "10:00"
-        },
-        {
-          place: "Kew Gardens",
-          address: "Royal Botanic Gardens, Kew, Richmond, Surrey, TW9 3AE, United Kingdom",
-          description: "A botanical garden with the largest and most diverse botanical collections in the world.",
-          info: "Tickets can be purchased online or at the venue.",
-          link: "https://www.kew.org/",
-          time: "13:00"
-        }
-      ]
-    },
-    4:{
-      day: 4,
-      date: "23-09-2024",
-      title: "Water Sports and Activities",
-      activities: [
-        {
-          place: "WakeUp Docklands",
-          address: "Royal Victoria Beach, Western Gateway, Royal Docks, London E16 1FA, United Kingdom",
-          description: "A water sports center offering activities like wakeboarding and paddleboarding.",
-          info: "Tickets can be purchased online or at the venue.",
-          link: "https://wakeupdocklands.com/",
-          time: "10:00"
-        },
-        {
-          place: "Lee Valley White Water Centre",
-          address: "Station Road, Waltham Cross, Hertfordshire, EN9 1AB, United Kingdom",
-          description: "An outdoor center offering white water rafting and kayaking.",
-          info: "Tickets can be purchased online or at the venue.",
-          link: "https://www.visitleevalley.org.uk/en/content/cms/outdoors/white-water-centre/",
-          time: "13:00"
-        }
-      ]
-    },
-    5:{
-      day: 5,
-      date: "24-09-2024",
-      title: "Sightseeing",
-      activities: [
-        {
-          place: "Buckingham Palace",
-          address: "Westminster, London SW1A 1AA, United Kingdom",
-          description: "The London residence and administrative headquarters of the monarch of the United Kingdom.",
-          info: "Tickets can be purchased online or at the venue. Note that it is open to the public only during certain times of the year.",
-          link: "https://www.rct.uk/visit/the-state-rooms-buckingham-palace",
-          time: "10:00"
-        },
-        {
-          place: "London Eye",
-          address: "Riverside Building, County Hall, London SE1 7PB, United Kingdom",
-          description: "A large observation wheel on the South Bank of the River Thames in London.",
-          info: "Tickets can be purchased online or at the venue.",
-          link: "https://www.londoneye.com/",
-          time: "13:00"
-        }
-      ]
-    }}
-  };
+    // 3:{
+    //   day: 3,
+    //   date: "22-09-2024",
+    //   title: "Nature and Outdoors",
+    //   activities: [
+    //     {
+    //       place: "Hyde Park",
+    //       address: "Hyde Park, London, United Kingdom",
+    //       description: "One of London's largest and most famous parks.",
+    //       info: "Free entry.",
+    //       link: "https://www.royalparks.org.uk/parks/hyde-park",
+    //       time: "10:00"
+    //     },
+    //     {
+    //       place: "Kew Gardens",
+    //       address: "Royal Botanic Gardens, Kew, Richmond, Surrey, TW9 3AE, United Kingdom",
+    //       description: "A botanical garden with the largest and most diverse botanical collections in the world.",
+    //       info: "Tickets can be purchased online or at the venue.",
+    //       link: "https://www.kew.org/",
+    //       time: "13:00"
+    //     }
+    //   ]
+    // },
+    // 4:{
+    //   day: 4,
+    //   date: "23-09-2024",
+    //   title: "Water Sports and Activities",
+    //   activities: [
+    //     {
+    //       place: "WakeUp Docklands",
+    //       address: "Royal Victoria Beach, Western Gateway, Royal Docks, London E16 1FA, United Kingdom",
+    //       description: "A water sports center offering activities like wakeboarding and paddleboarding.",
+    //       info: "Tickets can be purchased online or at the venue.",
+    //       link: "https://wakeupdocklands.com/",
+    //       time: "10:00"
+    //     },
+    //     {
+    //       place: "Lee Valley White Water Centre",
+    //       address: "Station Road, Waltham Cross, Hertfordshire, EN9 1AB, United Kingdom",
+    //       description: "An outdoor center offering white water rafting and kayaking.",
+    //       info: "Tickets can be purchased online or at the venue.",
+    //       link: "https://www.visitleevalley.org.uk/en/content/cms/outdoors/white-water-centre/",
+    //       time: "13:00"
+    //     }
+    //   ]
+    // },
+    // 5:{
+    //   day: 5,
+    //   date: "24-09-2024",
+    //   title: "Sightseeing",
+    //   activities: [
+    //     {
+    //       place: "Buckingham Palace",
+    //       address: "Westminster, London SW1A 1AA, United Kingdom",
+    //       description: "The London residence and administrative headquarters of the monarch of the United Kingdom.",
+    //       info: "Tickets can be purchased online or at the venue. Note that it is open to the public only during certain times of the year.",
+    //       link: "https://www.rct.uk/visit/the-state-rooms-buckingham-palace",
+    //       time: "10:00"
+    //     },
+    //     {
+    //       place: "London Eye",
+    //       address: "Riverside Building, County Hall, London SE1 7PB, United Kingdom",
+    //       description: "A large observation wheel on the South Bank of the River Thames in London.",
+    //       info: "Tickets can be purchased online or at the venue.",
+    //       link: "https://www.londoneye.com/",
+    //       time: "13:00"
+    //     }
+    //   ]
+    // }
+  }};
+  
+  async function getProgramWithObservation(method, actualProgram, observation){
+    let obNou = {};
+    actualProgram.forEach((ob, index)=>{
+      obNou[index + 1] = ob;
+    });
+    const objectProgram = {"program" : obNou};
+    axios.post(`${address_function_program}`, {method, program: objectProgram, observation}).then((data)=>{
+      console.log(data.data)
+    }).catch((err)=>{
+      console.log(err);
+    })
+
+  }
+
+
+  async function getProgram( method, from, to, city, country, newCheckbox){
+    axios.post(`${address_function_program}`, 
+      {from, to, city, country, newCheckbox, method}
+    ).then((data)=>{
+      if(data.data.type){
+        const values = Object.values(data.data.data);
+        setProgram([...values]);
+      }else{
+        props.addNotification("warning", "Unfortunately, we could not generate your program.")
+      }       
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
   
 
+  setTimeout(() => {
+    getProgramWithObservation("observation", program, "I want you to chenge the start day trip to 31-12-2023")
+
+  }, 4000);
 
     
-  useEffect( ()=>{
+  useEffect(()=>{
     const from = '20-09-2024';
     const to = '21-09-2024';
     const city = 'London';
     const country = 'England';
     const newCheckbox = ['Explore skyscrapers and modern architecture', 'Enjoy desert safari and camel riding', 'Visit cultural heritage sites and museums', "Nature and outdoors", 'Try water sports and activities', "sightseeing"];
 
-
-    setProgram([...Object.values(prog.program)]);
-
-    
     // const {from, to, city, country, checkbox} = props.route.params;
     // console.log({from, to, city, country, checkbox});
 
     // let newCheckbox =[];
     // checkbox.forEach((ob)=>{if(ob.selected)newCheckbox.push(ob.category)});
 
+    setProgram([...Object.values(prog.program)]);
 
 
-    // axios.post(`${address_function_program}`, 
-    //   {from, to, city, country, newCheckbox}
-    // ).then((data)=>{
-    //   if(data.data.type){
-    //     const values = Object.values(data.data.data);
-    //     setProgram([...values]);
-    //   }else{
-    //     props.addNotification("warning", "Unfortunately, we could not generate your program.")
-    //   }       
-    // }).catch((err)=>{
-    //     console.log(err);
-    // })
+    // getProgram('create', from, to, city, country, newCheckbox)
 
-
+  
   }, []);
 
 
@@ -186,7 +208,6 @@ const Program = (props) => {
       <View> 
       {program.map((ob, index)=>{
 
-        console.log(ob);
         return  <Card  key={index}  p="$5" borderRadius="$lg" maxWidth={360} m="$3">
 
           <Text fontSize="$sm"  fontStyle="normal"  fontFamily="$heading"  fontWeight="$normal"  lineHeight="$sm"  mb="$2"
@@ -213,6 +234,16 @@ const Program = (props) => {
         </Card>   
       })}
       </View>}
+
+
+
+      <View style={styles.viewInputObservation} >
+        <TextInput  style={styles.inputObseravation}
+          multiline
+          onChangeText={(text) => setObservation(text)}
+          value={observation}
+        />
+      </View>
     </ScrollView>
   )
 }
@@ -221,6 +252,18 @@ export default Program
 
 const styles = StyleSheet.create({
  
+  viewInputObservation: {
+    margin:20
+  },
+  inputObseravation: {
+    height: 100, 
+    borderColor: '#ccc', 
+    borderWidth: 1, 
+    borderRadius: 8, 
+    padding: 10, 
+    textAlignVertical: 'top', 
+    backgroundColor: '#f9f9f9'
+  },
   container: {
     marginTop: 300
   }
@@ -252,4 +295,4 @@ const styles = StyleSheet.create({
 
 // Intrebari: 
 // Unde as putea sa adaug butonul acceptare sau refuzare a calatoriei 
-
+// Text input car eridica si tastarura dar si inputul 
--- diverse.js
+++ diverse.js
@@ -19,43 +19,4 @@ function formatDateFromMilliseconds(milliseconds) {
 }
 
 
-async function isValidLink(link){
-    try{
-        let rez = false;
-        let data = await   fetch(link);
-        if(data.status === 200)rez = true;
-        return rez;
-    }catch(err){
-        return false;
-    }
-}
-
-
-async function checkWebsites(content) {
-    const newProgram = {};
-
-    try{
-        for (const dayNumber of Object.keys(content.program)) {
-            const day = content.program[dayNumber];
-            const { activities } = day;
-            const newActivities = await Promise.all(
-              activities.map(async (ob) => {
-                const link = ob.link;
-                const rez = await isValidLink(link);
-                if (rez) {
-                  return ob;
-                } else {
-                  return { ...ob, link: '' };
-                }
-              })
-            );
-            newProgram[dayNumber] = { ...day, activities: newActivities };
-        }
-    }catch(err){
-        console.log('Avem eroare', err);
-    }
-  
-    return newProgram;
-}
-
-export {formatDateFromMilliseconds, checkWebsites,  address_function_program, address_function_fuzzy, address_function_checkbox, address_function_testtime }
+export {formatDateFromMilliseconds,  address_function_program, address_function_fuzzy, address_function_checkbox, address_function_testtime }
