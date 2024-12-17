dee4377fd250823bc78aab61c45e8a67ebf2c56a
HabaAndrei <habaconstantin45@gmail.com>
2024-12-17 19:55:13 +0200

    17-12-2024

--- App.js
+++ App.js
@@ -62,6 +62,11 @@ const App = () => {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
+            name="Locations"
+            component={customComponent(Locations)}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
             name="Chat"
             component={customComponent(Chat)}
             options={{headerShown: false}}
@@ -74,11 +79,6 @@ const App = () => {
           <Stack.Screen
             name="MyTrips"
             component={customComponent(MyTrips)}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Locations"
-            component={customComponent(Locations)}
             options={{headerShown: false}}
           />
           <Stack.Screen
