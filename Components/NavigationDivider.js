0a617953c7c526aae09143d0adbb6b8a344f46cb
Haba Andrei <habaconstantin45@gmail.com>
2025-03-27 21:36:18 +0200

    put in center the natigation divider

--- Components/NavigationDivider.js
+++ Components/NavigationDivider.js
@@ -1,24 +1,34 @@
-import { View } from 'react-native'
+import { View } from 'react-native';
 import { HStack, Text, Divider, ArrowLeftIcon, CheckIcon, Icon } from '@gluestack-ui/themed';
 
 const NavigationDivider = (props) => {
   return (
     <View>
-      <HStack h="$10" justifyContent="center" alignItems="center">
-        <HStack alignItems="center"  >
+      <HStack h="$10" alignItems="center">
+        <HStack alignItems="center" flex={1} justifyContent="flex-end">
           <Icon as={ArrowLeftIcon} m="$2" w="$6" h="$6" />
-          <Text bold={true} onPress={()=>props.firstFunction()} >{props.firstFunctionName}</Text>
+          <Text bold onPress={() => props.firstFunction()}>
+            {props.firstFunctionName}
+          </Text>
         </HStack>
 
-        <Divider  style={{ margin: 15 }}  orientation="vertical"  mx="$2.5"  bg="$indigo500"  h={25}  $dark-bg="$indigo400"/>
+        <Divider
+          orientation="vertical"
+          bg="$indigo500"
+          h={25}
+          mx="$3"
+          alignSelf="center"
+        />
 
-        <HStack alignItems="center">
-          <Text bold={true} onPress={()=>props.secondFunction()} >{props.secondFunctionName}</Text>
+        <HStack alignItems="center" flex={1} justifyContent="flex-start">
+          <Text bold onPress={() => props.secondFunction()}>
+            {props.secondFunctionName}
+          </Text>
           <Icon as={CheckIcon} m="$2" w="$6" h="$6" />
         </HStack>
       </HStack>
     </View>
-  )
-}
+  );
+};
 
 export default NavigationDivider;
